f65906b45e57698c848ccef0fadf26845067ede4
924322786 <akaattarilim@gmail.com>
2026-04-27 09:26:44 -0700

    epsilon 0.1

--- cifar10-fedrl/dqn_agent.py
+++ cifar10-fedrl/dqn_agent.py
@@ -29,7 +29,7 @@ class DQN_Agent:
 
         # self.epsilon = self.eps_start
 
-        self.epsilon = 0.4
+        self.epsilon = 0.1
     # def update_epsilon(self):
     #     frac = min(self.episode / self.eps_decay_episodes, 1.0)
     #     self.epsilon = (
